16c4eae9b53a6e521d3272da2c339bcfe12c5a25
kuraimos <kuraimoss@gmail.com>
2024-06-22 18:38:54 +0700

    update

--- lib/body_component/pesanan.dart
+++ lib/body_component/pesanan.dart
@@ -88,84 +88,309 @@ class _PesananPageState extends State<PesananPage>
       ),
     );
   }
-Widget _buildProsesTab() {
-  return Padding(
-    padding: const EdgeInsets.all(14.0),
-    child: Column(
+
+  Widget _buildProsesTab() {
+    return Padding(
+      padding: const EdgeInsets.all(14.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildPesananProsesItem(
+            'assets/machiato.jpg',
+            'American Coffee, Ekspresso',
+            '2 Item',
+            'Rp. 50.000',
+            '08/1/2024 21.00',
+            'Proses',
+            'Pick Up',
+            Color(0xFF107d72),
+          ),
+          Divider(color: Colors.grey),
+          _buildPesananProsesItem(
+            'assets/machiato.jpg',
+            'American Coffee, Ekspresso',
+            '2 Item',
+            'Rp. 50.000',
+            '08/1/2024 21.00',
+            'Proses',
+            'Delivery',
+            Color(0xFFbd5c17),
+          ),
+          Divider(color: Colors.grey),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPesananProsesItem(
+      String imagePath,
+      String title,
+      String quantity,
+      String price,
+      String dateTime,
+      String prosesText,
+      String statusText,
+      Color statusColor) {
+    return Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              flex: 2,
-              child: Row(
+        Expanded(
+          flex: 2,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                imagePath,
+                width: 80,
+                height: 80,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(width: 8),
+              Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Image.asset(
-                    'assets/machiato.jpg', 
-                    width: 80,
-                    height: 80,
-                    fit: BoxFit.cover,
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 11,
+                    ),
                   ),
-                  SizedBox(width: 8), 
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  SizedBox(height: 4),
+                  Text(
+                    quantity,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Row(
                     children: [
-                      Text(
-                        'American Coffee, Ekspresso', 
-                        style: TextStyle(
-                          fontSize: 11,
-                        ),
+                      Icon(
+                        Icons.access_time_filled,
+                        color: Colors.black,
+                        size: 13,
                       ),
-                      SizedBox(height: 4), 
+                      SizedBox(width: 4),
                       Text(
-                        '2 Item',
+                        prosesText,
                         style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                          fontSize: 13,
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
                     ],
                   ),
-                ],
-              ),
-            ),
-            SizedBox(width: 16), // Jarak antara kolom
-            Expanded(
-              flex: 1,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    'Rp. 50.000', // Harga
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+                  Container(
+                    decoration: BoxDecoration(
+                      color: statusColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    child: Row(
+                      children: [
+                        SizedBox(width: 4),
+                        Text(
+                          statusText,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-        SizedBox(height: 16), // Jarak antara baris
-        Divider(color: Colors.grey), // Divider di bawah konten
+        SizedBox(width: 16), // Jarak antara kolom
+        Expanded(
+          flex: 1,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                price, // Harga
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 40),
+              Text(
+                dateTime, // Tanggal dan waktu
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
+        ),
       ],
-    ),
-  );
-}
-
-
-
+    );
+  }
 
   Widget _buildSelesaiTab() {
-    // Placeholder untuk tab "Selesai"
-    return Center(
-      child: Text(
-        'Halaman Pesanan Selesai',
-        style: TextStyle(fontSize: 20),
+    return Padding(
+      padding: const EdgeInsets.all(14.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildPesananSelesaiItem(
+            'assets/machiato.jpg',
+            'American Coffee, Ekspresso',
+            '2 Item',
+            'Rp. 50.000',
+            '08/1/2024 21.00',
+            'Selesai',
+            'Pick Up',
+            Color(0xFF107d72),
+          ),
+          Divider(color: Colors.grey),
+          _buildPesananSelesaiItem(
+            'assets/machiato.jpg',
+            'American Coffee, Ekspresso',
+            '2 Item',
+            'Rp. 50.000',
+            '08/1/2024 21.00',
+            'Dibatalkan',
+            'Delivery',
+            Color(0xFFbd5c17),
+          ),
+          Divider(color: Colors.grey),
+        ],
       ),
+    );
+  }
+
+  Widget _buildPesananSelesaiItem(
+    String imagePath,
+    String title,
+    String quantity,
+    String price,
+    String dateTime,
+    String prosesText,
+    String statusText,
+    Color statusColor,
+  ) {
+    IconData iconData;
+    switch (prosesText) {
+      case 'Selesai':
+        iconData = Icons.check_circle;
+        break;
+      case 'Dibatalkan': // Add more cases if needed for different icons
+        iconData = Icons.cancel;
+        break;
+      default:
+        iconData = Icons.check_circle; // Default to a sensible icon
+    }
+
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          flex: 2,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                imagePath,
+                width: 80,
+                height: 80,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(width: 8),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 11,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    quantity,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Row(
+                    children: [
+                      Icon(
+                        iconData, // Use the dynamically determined iconData
+                        color: Colors.black,
+                        size: 13,
+                      ),
+                      SizedBox(width: 4),
+                      Text(
+                        prosesText,
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 13,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: statusColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    child: Row(
+                      children: [
+                        SizedBox(width: 4),
+                        Text(
+                          statusText,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        SizedBox(width: 16), // Jarak antara kolom
+        Expanded(
+          flex: 1,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                price, // Harga
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 40),
+              Text(
+                dateTime, // Tanggal dan waktu
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
